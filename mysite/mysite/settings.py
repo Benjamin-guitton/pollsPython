d068bd1c53cf5ccead0c7ae861a6b845d93b0607
Benjamin <benjamin.guittons@outlook.com>
2022-05-03 09:28:45 +0200

    Ajout quasi fin

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i_gdts(zpmi(7ra82!_um*jbm2_igi5re0brz0a(=i&o&gba&p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
 
 # Application definition
